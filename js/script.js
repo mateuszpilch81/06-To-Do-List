719a9c70f233718c3e4324894d9f1a99186eea6e
Mateusz Pilch <mp@mkpilch.pl>
2022-12-31 13:08:43 +0100

    add code

--- js/script.js
+++ js/script.js
@@ -70,3 +70,13 @@
 
  newTaskElement.focus();
 };
+
+const init = () => {
+    render ();
+
+    const form = document.querySelector (".js-form");
+    form.addEventListener ("submit", onFormSubmit);
+
+};
+init ();
+    }
