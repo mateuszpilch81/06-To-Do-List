93f9bc015628f610a82716ad53729407248e1f2b
Mateusz Pilch <mp@mkpilch.pl>
2022-12-31 12:59:26 +0100

    add code

--- js/script.js
+++ js/script.js
@@ -9,5 +9,51 @@
         task[taskIndex].done = !task[taskIndex].done;
         rednder ();
     };
-    
-}
+
+    const addNewTask = (newTaskContent) => {
+        task.push({content: newTaskContent})
+        render();
+    };
+
+    const bindRemoveEvents = () => {
+        const removeButtons  = document.querySelectorAll (".js-remove");
+
+        removeButtons.forEach((removeButton, taskIndex) => {
+            removeButton.addEventListener ("click", () => {
+                removeTask(taskIndex);
+            });
+        });
+    };
+
+    const bindToogleDoneEvents = () => {
+        const toggleDoneButtons = document.querySelectorAll (".js-toggleDone");
+
+        toggleDoneButtons.forEach((toggleDoneButtons, taskIndex) => {
+        toggleDoneButtons.addEventListener ("click", () => {
+            toggleTaskDone (taskIndex);
+        });
+    });
+    };
+
+    const render = () => {
+        lettaskListHTMLContent = "";
+        for (const task of tasks) {
+            taskListHTMLContent += '
+            <li class "tasks__item js-task">
+
+            <button class = "tasks__button tasks__button--toogleDone js-toggleDone">
+            ${task.done ? "✓" : ""}
+            </button>
+<span class = "tasks__content ${task.done ? " tasks__content--done" : ""} " >
+ ${task.content}
+  </span>
+<button class = "tasks__button tasks__button--remove js-remove">
+🗑
+</button>
+</li>
+';
+        }
+        document.querySelector(".js-tasks).innerHTML = taskListHTMLContent;
+        bindRemoveEvents();
+        bindToggleDoneEvents();
+    };
