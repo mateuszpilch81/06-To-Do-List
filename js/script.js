d0cbeb1b9d7add0b83269a254697fe46d253123f
Mateusz Pilch <mp@mkpilch.pl>
2023-01-01 20:26:17 +0100

    change code

--- js/script.js
+++ js/script.js
@@ -46,7 +46,7 @@
       <button class="tasks__button tasks__button--toggleDone js-toggleDone">
       ${task.done ? "✓" : ""}
       </button>
-      <span class="tasks__item${task.done ? " tasks__item--done" : ""}">${task.content}</span>
+      <span class="tasks__content${task.done ? " tasks__content--done" : ""}">${task.content}</span>
       <button class="tasks__button tasks__button--remove js-remove">
       🗑
       </button>
